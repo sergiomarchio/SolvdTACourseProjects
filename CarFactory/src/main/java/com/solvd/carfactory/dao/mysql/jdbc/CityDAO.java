42084483a478c36ed74c1a7417096c95c5314698
Sergio Marchio <sergiomarchio@gmail.com>
2021-08-04 01:17:31 -0300

    completed CRUD for country DAO

--- CarFactory/src/main/java/com/solvd/carfactory/dao/mysql/jdbc/CityDAO.java
+++ CarFactory/src/main/java/com/solvd/carfactory/dao/mysql/jdbc/CityDAO.java
@@ -6,27 +6,40 @@ import com.solvd.carfactory.models.location.City;
 import com.solvd.carfactory.models.location.Country;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CityDAO extends AbstractMysqlJdbcDAO implements ICityDAO {
     private final static Logger LOGGER = Logger.getLogger(CityDAO.class);
     private final static String GET_CITY_BY_ID = "SELECT * FROM cities WHERE id = ?";
+    private final static String SET_CITY_FROM_OBJECT = "INSERT INTO cities(name, country_id) VALUES (?), (?)";
 
     @Override
-    public void createItem(City item) {
+    public void createItem(City city) {
+        Connection connection = ConnectionPool.getInstance().getConnection();
 
+        try (PreparedStatement ps = connection.prepareStatement(SET_CITY_FROM_OBJECT,
+                Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, city.getName());
+            ps.setLong(1, city.getCountry().getId());
+            ps.executeQuery();
+
+            long id;
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                id = rs.getLong(1);
+            }
+            city.setId(id);
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        }
     }
     @Override
     public City getItemById(long id) {
         Connection connection = ConnectionPool.getInstance().getConnection();
 
-        try(PreparedStatement ps = connection.prepareStatement(GET_CITY_BY_ID)) {
+        try (PreparedStatement ps = connection.prepareStatement(GET_CITY_BY_ID)) {
             ps.setLong(1, id);
 
-            try(ResultSet rs = ps.executeQuery()){
+            try (ResultSet rs = ps.executeQuery()) {
                 rs.next();
                 return buildCity(rs);
             }
@@ -45,7 +58,7 @@ public class CityDAO extends AbstractMysqlJdbcDAO implements ICityDAO {
 
     }
 
-    private City buildCity(ResultSet rs) throws SQLException{
+    private City buildCity(ResultSet rs) throws SQLException {
         City c = new City();
 
         c.setId(rs.getLong("id"));
--- CarFactory/src/main/java/com/solvd/carfactory/dao/mysql/jdbc/CountryDAO.java
+++ CarFactory/src/main/java/com/solvd/carfactory/dao/mysql/jdbc/CountryDAO.java
@@ -5,19 +5,34 @@ import com.solvd.carfactory.dao.ICountryDAO;
 import com.solvd.carfactory.models.location.Country;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CountryDAO extends AbstractMysqlJdbcDAO implements ICountryDAO {
     private final static Logger LOGGER = Logger.getLogger(CountryDAO.class);
     private final static String GET_COUNTRY_BY_ID = "SELECT * FROM countries WHERE id = ?";
+    private final static String SET_COUNTRY_FROM_OBJECT = "INSERT INTO countries(name) VALUES (?)";
+    private final static String UPDATE_COUNTRY = "UPDATE countries SET name = ? WHERE id = ?";
+    private final static String DELETE_COUNTRY = "DELETE FROM countries WHERE id = ?";
 
     @Override
-    public void createItem(Country item) {
+    public void createItem(Country country) {
+        Connection connection = ConnectionPool.getInstance().getConnection();
 
+        try(PreparedStatement ps = connection.prepareStatement(SET_COUNTRY_FROM_OBJECT,
+                Statement.RETURN_GENERATED_KEYS)){
+            ps.setString(1, country.getName());
+            ps.executeUpdate();
+
+            long id;
+            try(ResultSet rs = ps.getGeneratedKeys()){
+                id = rs.getLong(1);
+            }
+            country.setId(id);
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        }
     }
+
     @Override
     public Country getItemById(long id) {
         Connection connection = ConnectionPool.getInstance().getConnection();
@@ -27,12 +42,7 @@ public class CountryDAO extends AbstractMysqlJdbcDAO implements ICountryDAO {
 
             try(ResultSet rs = ps.executeQuery()){
                 rs.next();
-                Country c = new Country();
-
-                c.setId(rs.getLong("id"));
-                c.setName(rs.getString("name"));
-
-                return c;
+                return buildCountry(rs);
             }
         } catch (SQLException e) {
             LOGGER.error(e);
@@ -40,12 +50,36 @@ public class CountryDAO extends AbstractMysqlJdbcDAO implements ICountryDAO {
 
         return null;
     }
-    @Override
-    public void updateItem(Country item) {
 
+    @Override
+    public void updateItem(Country country) {
+        Connection connection = ConnectionPool.getInstance().getConnection();
+
+        try(PreparedStatement ps = connection.prepareStatement(UPDATE_COUNTRY)){
+            ps.setString(1, country.getName());
+            ps.setLong(2, country.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        }
     }
+
     @Override
     public void deleteItem(long id) {
+        Connection connection = ConnectionPool.getInstance().getConnection();
 
+        try(PreparedStatement ps = connection.prepareStatement(DELETE_COUNTRY)){
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error(e);
+        }
+    }
+
+    private Country buildCountry(ResultSet rs) throws SQLException{
+        Country c = new Country();
+        c.setId(rs.getLong("id"));
+        c.setName(rs.getString("name"));
+        return c;
     }
 }
